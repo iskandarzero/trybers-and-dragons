b71a58b32e009c458755172c03e89edc22d7cebd
iskandarzero <riderzerotg@gmail.com>
2022-09-24 19:04:22 -0300

    reafctor: _lifePoints is declared as a protected attribute

--- src/Monster.ts
+++ src/Monster.ts
@@ -1,7 +1,7 @@
 import SimpleFighter from './Fighter/SimpleFighter';
 
 export default class Monster implements SimpleFighter {
-  private _lifePoints: number;
+  protected _lifePoints: number;
   private _strength: number;
 
   constructor() {
